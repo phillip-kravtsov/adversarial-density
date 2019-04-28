82b5348021cce3f3a68af04c2d8f4f285597ca69
Ubuntu <ubuntu@ip-172-31-47-118.us-west-2.compute.internal>
2019-04-28 02:26:22 +0000

    Added adversarial attack code

--- adversarial.py
+++ adversarial.py
@@ -1,27 +1,101 @@
+import argparse
+import os
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from train_resnet import get_cifar10_data
+from resnet import ResNet18
 
 def fgsm(image, epsilon, data_grad):
+
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon * sign_data_grad
     perturbed_image = torch.clamp(perturbed_image, 0, 1)
+
     return perturbed_image
 
-def get_adversarial_images(model, device, test_loader, epsilon, targeted=False):
-    advs = []
+def get_adversarial_images(model, device, test_loader, num_update_steps, 
+        num_images, epsilon, targeted=False):
+
+    assert 1 <= num_images <= 10000
+
+    adversarials = []
+    original_classes = []
+
+    num_images_completed = 0
+
     for data, target in test_loader:
+
         data, target = data.to(device), target.to(device)
         data.requires_grad = True
-        output = model(data)
-        init_pred = output.max(1, keepdim=True)[1]
-        if targeted:
-            raise NotImplementedError
-        else:   
-            loss = F.nll_loss(output, target)
-        model.zero_grad()
-        loss.backward()
-        data_grad = data.grad.data
-        perturbed_data = fgsm(data, epsilon, data_grad)
-        advs.append(perturbed_data.squeeze().cpu().detach().numpy())
-    return advs
+
+        print('Starting new batch of attacks ...')
+
+        for _ in range(num_update_steps):
+            
+            data = torch.cuda.FloatTensor(data.detach())
+            data.requires_grad = True
+            output = model(data)
+            probs = torch.log_softmax(output, dim=1)
+
+            if targeted:
+                raise NotImplementedError
+            else:
+                loss = F.nll_loss(probs, target)
+                print(loss.item())
+
+            data.grad = None
+            model.zero_grad()
+            loss.backward()
+            data_grad = data.grad.data
+            data = fgsm(data, epsilon, data_grad)
+
+        adversarials.append(data.squeeze().cpu().detach().numpy())
+        original_classes.append(target.cpu().detach().numpy())
+
+        num_images_completed += data.shape[0]
+        if num_images_completed >= num_images:
+            break
+
+    adversarials = np.concatenate(adversarials)
+    original_classes = np.concatenate(original_classes)
+    adversarials = adversarials[:num_images]
+    original_classes = original_classes[:num_images]
+
+    return adversarials, original_classes
+
+def do_adversarial(args):
+
+    device = torch.device('cuda')
+
+    model = ResNet18().to(device)
+    model.load_state_dict(torch.load(args.resnet_fname))
+
+    _, _, test_loader = get_cifar10_data(bs=args.bs, as_loader=True)
+    adversarials, original_classes = get_adversarial_images(
+        model, torch.device('cuda'), test_loader, args.num_update_steps,
+        args.num_images, args.epsilon, targeted=False,
+    )
+    to_save = {'adversarials': adversarials, 'original_classes': original_classes}
+    torch.save(to_save, args.save_fname)
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epsilon', type=float, required=True)
+    parser.add_argument('--num-update-steps', type=int, required=True)
+    parser.add_argument('--num-images', type=int, required=True)
+    parser.add_argument('--save-fname', type=str, required=True)
+    parser.add_argument('--resnet-fname', type=str, required=False,
+        default='./logdirs/cifar10-40k_best.pt')
+    parser.add_argument('--bs', type=int, required=False, default=256)
+    args = parser.parse_args()
+
+    print(args)
+    do_adversarial(args)
         
+if __name__ == '__main__':
+    main()
 
---
+++ adversarial_all.sh
@@ -0,0 +1,3 @@
+python adversarial.py --num-update-steps 10 --num-images 1000 --epsilon 0.0001 --save-fname data/adversarial/untargeted_step10_eps0.0001.pt 
+python adversarial.py --num-update-steps 10 --num-images 1000 --epsilon 0.001 --save-fname data/adversarial/untargeted_step10_eps0.001.pt 
+python adversarial.py --num-update-steps 10 --num-images 1000 --epsilon 0.01 --save-fname data/adversarial/untargeted_step10_eps0.01.pt 
